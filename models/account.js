9fcaba17889ba494b684b8f7367efeb3911ecfa7
Ramakotireddy9505 <rkotireddyragipindi4b6@gmail.com>
2023-11-29 21:16:22 -0600

    bonus

--- models/account.js
+++ models/account.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
+
 const accountSchema = new Schema({
-    username: String,
-    password: String
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 20
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6  // You can adjust this based on your requirements
+  }
 });
+
 accountSchema.plugin(passportLocalMongoose);
+
 // We export the Schema to avoid attaching the model to the
 // default mongoose connection.
 module.exports = mongoose.model("Account", accountSchema);
